ba1ad427bdcb1daa99b0cfea23e3e615a7a70585
asfiya12 <asfiya1294@gmail.com>
2024-03-11 20:24:58 +0530

    changes made

--- WsSoapPublish/src/main/java/com/ws/soup1/HelloWorld.java
+++ WsSoapPublish/src/main/java/com/ws/soup1/HelloWorld.java
@@ -2,8 +2,10 @@ package com.ws.soup1;
 
 import jakarta.jws.WebMethod;
 import jakarta.jws.WebService;
+import jakarta.jws.soap.SOAPBinding;
 
 @WebService
+@SOAPBinding(style=SOAPBinding.Style.RPC)
 public interface HelloWorld {
 	@WebMethod
 	String getHelloWorld(String name);
